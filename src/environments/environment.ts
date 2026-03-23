9f6250f25ee2d77b0454cd3e24b5bd09c04c8cc7
asma828 <asmalachhab66@gmail.com>
2026-03-23 16:08:01 +0100

    chore: setup environment variables

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:8080/api',
+  stripePublicKey: 'pk_test_51QywBQRMlpV96MOWb8BeKEXxUzBJ5ZK3TOICye01UQCljKInrdcuPvNK8TqMxQhriZ571bSr8EROUx4s3YnN6xjM00QRSWYHEy'
 };
